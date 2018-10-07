5433c6376a0f1df98f621ae096bdeeafdcdae7f2
jariazavalverde <joseriazavalverde@hotmail.com>
2018-10-08 00:12:43 +0200

    refactoring

--- src/sat.h
+++ src/sat.h
@@ -15,10 +15,6 @@
 int check_sat(Formula *F, Interpretation *I);
 /** Propagate a value of a variable */
 int replace_variable(Formula *F, Interpretation *I, Action *actions, Atom atom, Bool value);
-/** 1-Literal rule (unit propagation) */
-int unit_propagation(Formula *F, Interpretation *I, Action *actions);
-/** Positive-Negative rule */
-int positive_negative(Formula *F, Interpretation *I, Action *actions);
 /** Split cases */
 int split_cases(Formula *F, Interpretation *I, Action *actions);
 /** Remove a clause from a formula */
@@ -29,8 +25,6 @@ void remove_literal(Formula *F, Action *actions, Clause *clause, Atom atom);
 void add_clause(Formula *F, Clause *clause);
 /** Add a literal to a clause */
 void add_literal(Formula *F, Clause *clause, Atom atom, Literal literal);
-/** Update count of positive-negative literals in a formula */
-void update_count_positive_negative(Formula *F, Clause *clause, Operation op);
 /** Prepend a new action */
 void push_action(Action *actions, Clause *clause, Atom atom, Literal literal);
 /** Perform a backtracking */
