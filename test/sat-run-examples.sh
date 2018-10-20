46e82fb1f5f24a76d49d795e0e251e1ee49ace61
jariazavalverde <joseriazavalverde@hotmail.com>
2018-10-20 21:00:49 +0200

    improved sat-run-examples test with timeout

--- test/sat-run-examples.sh
+++ test/sat-run-examples.sh
@@ -1,39 +1,63 @@
 #!/bin/bash
 
-# Compile source files and generate an executable
-echo "Compiling source files..."
+# COMPILE SOURCE FILES AND GENERATE AN EXECUTABLE
+echo -e "\e[39mCompiling source files..."
 c=`gcc -I/usr/include -L/usr/lib ../src/main.c ../src/io.h ../src/io.c ../src/structures.c ../src/structures.h ../src/sat.h ../src/sat.c -o sat`
 
+# If there is a fail in compilation, exit
 if [ "$c" != "" ] ; then
-	echo "FAIL!"
+	echo -e "\e[39mfail!"
 	exit
 else
-	echo "DONE!"
+	echo -e "\e[39mdone!"
 fi
 
-# Test SAT programs from /examples directory
-echo "Testing SAT programs..."
+# TEST SAT PROGRAMS FROM /examples DIRECTORY
+echo -e "\e[39mTesting SAT programs..."
+
+# Number of tests
 i=0
+# Total number of errors
 errors=0
+# Total number of success
 success=0
-for p in `ls ../examples/`; do
+# Total number of timeouts
+timeout=0
+
+# Iterate files from /examples directory
+for p in `find ../examples -name \*.cnf -print`; do
+
+	# Update number of files
 	i=$((i+1))
-	c=`./sat "../examples/$p"`
-	s=`echo $c | cut -d$' ' -f1`
-	m=`echo .$c | cut -d$' ' -f2-`
-    echo "....($i) $p"
-	if [ "$s" != "SAT" ] && [ "$s" != "UNSAT" ] ; then
-		echo "........FAIL!"
-		errors=$((errors+1))
-	else
-		echo "........$s!"
-		if [ "$s" = "SAT" ] ; then
-			echo "........($m)"
+	
+	# Run SAT solver with timeout
+	c=`timeout ${1:-1}s ./sat "$p"`
+	
+	# If SAT program ends
+	if [ "$c" != "" ] ; then
+		# Get satisfiability status (SAT or UNSAT)
+		s=`echo $c | cut -d$' ' -f1`
+		# Get model (if exists)
+		m=`echo .$c | cut -d$' ' -f2-`
+		# Print status (ERROR, SAT or UNSAT)
+		if [ "$s" != "SAT" ] && [ "$s" != "UNSAT" ] ; then
+			echo -e "\e[39m($i) \e[31m[$p ERROR!\e[39m"
+			errors=$((errors+1))
+		else
+			echo -e "\e[39m($i) \e[32m$p $s!\e[39m"
+			success=$((success+1))
 		fi
-		echo "........OK!"
-		success=$((success+1))
+	# If SAT program doesn't end
+	else
+		echo -e "\e[39m($i) \e[96m$p TIMEOUT!\e[39m"
+		timeout=$((timeout+1))
 	fi
+	
 done
-echo "$success SUCCESS!"
-echo "$errors ERRORS!"
-echo "DONE!"
+
+echo -e "\e[39mdone!\e[39m"
+
+# Print statistics
+echo -e "\e[32m$success SUCCESS!\e[39m"
+echo -e "\e[31m$errors ERRORS!\e[39m"
+echo -e "\e[96m$timeout TIMEOUTS!\e[39m"
